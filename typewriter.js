8fde283f353e79f2328f0f98b53e2e7b3eab5ca3
Calvin Zheng <calvinzheng08@hotmail.com>
2020-04-07 21:40:23 +0000

    fix

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,6 @@
-const sentence = "hello there from lighthouse labs \n"; 
-for (const [i, char] of sentence.split("").entries()) { //loop through and split to each char
+const sentence = "hello there from lighthouse labs \n";
+for (const [i, char] of sentence.split("").entries()) {
   setTimeout(() => {
-    console.log(char);  //console log each char
-  },i * 100) //timer 
+      process.stdout.write(char); 
+  },i *  50) 
   }
